e8f70a7e17505f6e578bc0b65b1a34c58cb36648
koppe555 <koppe55koppe@gmail.com>
2019-06-07 15:16:45 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <Header/>
+    <!-- ここを追加 -->
+    <main>
+      <router-view/>
+    </main>
+    <!-- ここを追加 -->
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+
+<style lang="stylus">
+@import url('https://fonts.googleapis.com/css?family=Quicksand&display=swap');
+
+body {
+  margin: 0;
+  font-size: 14px;
+  font-family: 'Quicksand', 'YuGothic', 'Yu Gothic', serif;
+}
+
+:link, :visited {
+  text-decoration: none;
+  color: inherit;
+}
+
+main {
+  margin: 80px auto 0;
+  width: 95%;
+  max-width: 600px;
+}
+
+.btns {
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+}
+
+button {
+  margin: 0 0 0 5px;
+  width: 40px;
+  height: 40px;
+  border: none;
+  border-radius: 5px;
+  background: #555;
+  background-size: cover;
+  color: white;
+  font-family: 'Quicksand', 'YuGothic', 'Yu Gothic', serif;
+  font-size: 1.2rem;
+  transition: 0.3s;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:focus {
+    outline: none;
+  }
+}
+
+.list {
+  box-shadow: 0 0 6px rgba(0, 0, 0, 0.1);
+  border-radius: 3px;
+  background: white;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUser)
+
+Vue.component('fa', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: function (h) { return h(App) }
+}).$mount('#app')
